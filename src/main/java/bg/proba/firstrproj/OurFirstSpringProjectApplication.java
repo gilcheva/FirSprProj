ee37f3424fd4c9c1120d3783ca5512c3534892d5
Galina Lambova <lambova@abv.bg>
2021-02-11 23:49:26 +0200

    Start and Book model

---
+++ src/main/java/bg/proba/firstrproj/OurFirstSpringProjectApplication.java
@@ -0,0 +1,13 @@
+package bg.proba.firstrproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurFirstSpringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OurFirstSpringProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bg/proba/firstrproj/OurFirstSpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.proba.firstrproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OurFirstSpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
